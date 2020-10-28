8b958f330a492d3e4cdd35a9b17e4747916dcba3
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2020-10-28 13:01:53 +0200

    Adding Employee View

--- EmployeeUnitOfWork.App/Models/EmployeeViewModel.cs
+++ EmployeeUnitOfWork.App/Models/EmployeeViewModel.cs
@@ -7,5 +7,9 @@ namespace EmployeeUnitOfWork.App.Models
 {
     public class EmployeeViewModel
     {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime HireDate { get; set; }
     }
 }
